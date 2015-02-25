8817eec850c3ae694055b08958be37172d9e0f2f
vtraag <vincent@traag.net>
2015-02-25 12:00:27 +0100

    Corrected MANIFEST and release code.

--- release.sh
+++ release.sh
@@ -10,4 +10,12 @@ python setup.py sdist upload --sign
 python2.7 setup.py bdist_egg upload --sign
 python3.4 setup.py bdist_egg upload --sign
 
+cd -
+
+if [ ! -d "dist/" ]; then
+  mkdir dist/
+fi
+
+cp /tmp/louvain/dist/* dist/
+
 rm -rf /tmp/louvain/
