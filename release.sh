0a5b2b053e3143d799539036388837ed7acd6606
vtraag <vincent@traag.net>
2015-02-25 12:09:56 +0100

    Changed to prevent accidental release. Uncomment in case of actual release. Make sure submission is working, if not a new release must be done.

--- release.sh
+++ release.sh
@@ -6,9 +6,10 @@ cp -R * /tmp/louvain/
 cd /tmp/louvain/
 
 # source dist 
-python setup.py sdist upload --sign
-python2.7 setup.py bdist_egg upload --sign
-python3.4 setup.py bdist_egg upload --sign
+python setup.py sdist
+#python setup.py sdist upload --sign
+#python2.7 setup.py bdist_egg upload --sign
+#python3.4 setup.py bdist_egg upload --sign
 
 cd -
 
