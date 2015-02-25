8aa6adcd4823664002deb8059b9b034d52837224
vtraag <vincent@traag.net>
2015-02-25 10:54:19 +0100

    Corrected MS setup_ms.py to build MS binary distributions.

--- win_build.bat
+++ win_build.bat
@@ -1,15 +1,15 @@
 REM Build for Python 2.7 32 bits
-c:\Python27\python.exe setup_ms.py --verbose bdist_msi
+c:\Python27\python.exe setup_ms.py --verbose bdist_msi --target-version=2.7
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Build for Python 2.7 64 bits
-c:\Python27-x64\python.exe setup_ms.py --verbose bdist_msi
+c:\Python27-x64\python.exe setup_ms.py --verbose bdist_msi --target-version=2.7
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Build for Python 3.4 32 bits
-c:\Python34\python.exe setup_ms.py --verbose bdist_msi
+c:\Python34\python.exe setup_ms.py --verbose bdist_msi --target-version=3.4
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Build for Python 3.4 64 bits
-c:\Python34-x64\python.exe setup_ms.py --verbose bdist_msi
+c:\Python34-x64\python.exe setup_ms.py --verbose bdist_msi --target-version=3.4
 if %errorlevel% neq 0 exit /b %errorlevel%
