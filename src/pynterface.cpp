231887c9f0d6db0ca6bbd77ff665eff5ad395896
vtraag <vincent@traag.net>
2015-12-04 12:12:02 +0100

    Included in python interface for testing.

--- src/pynterface.cpp
+++ src/pynterface.cpp
@@ -123,6 +123,9 @@ extern "C"
 
     Optimiser opt;
     opt.consider_comms = consider_comms;
+    opt.smart_local_move = true;
+    opt.move_individual = true;
+    opt.consider_empty_community = true;
 
     MutableVertexPartition* partition = create_partition_from_py(py_obj_graph, method, py_initial_membership, py_weights, resolution_parameter);
 
