fd899163d664871505d56d9ab89220d5014bb545
vtraag <vincent@traag.net>
2015-12-14 15:35:03 +0100

    Added partition functions compatible with previous version (should be adapted still).

--- src/functions.py
+++ src/functions.py
@@ -18,3 +18,188 @@ def _get_py_capsule(graph):
     return graph.__graph_as_capsule();
   else:
     return graph.__graph_as_cobject();
+
+class Layer:
+  """
+  This class makes sure that each layer is properly encoded for use in the
+  ``find_partition_multiplex`` function.
+
+  Keyword arguments:
+
+  graph
+    The graph for this layer.
+
+  method
+    The method used for this layer (see package documentation for details).
+
+  layer_weight
+    The weight used for weighing this layer in the overall quality.
+
+  initial_membership
+    The initial membership to start the optimization with.
+
+  weight
+    The edge attribute from which to use the weight for this layer.
+
+  resolution_parameter
+    The resolution parameter used for this layer.
+
+  """
+  def __init__(self, graph, method, layer_weight=1.0, initial_membership=None, weight=None, resolution_parameter=1.0):
+    self.graph = graph;
+    self.method = method;
+    self.layer_weight = layer_weight;
+    self.initial_membership = initial_membership;
+    self.weight = weight;
+    self.resolution_parameter = resolution_parameter;
+
+def __get_py_capsule(graph):
+  if PY3:
+    return graph.__graph_as_capsule();
+  else:
+    return graph.__graph_as_cobject();
+
+def find_partition_multiplex(layers, consider_comms=ALL_NEIGH_COMMS):
+  """
+  Method for detecting communities using the Louvain algorithm. This functions
+  finds the optimal partition for all layers given the specified methods. For
+  the various possible methods see package documentation. This considers all
+  graphs in all layers, in which each node may be differently connected, but all
+  nodes must appear in all graphs. Furthermore, they should have identical
+  indices in the graph (i.e. node i is assumed to be the same node in all
+  graphs).  The quality of this partition is simply the sum of the individual
+  qualities for the various partitions, weighted by the layer_weight. If we
+  denote by q_k the quality of layer k and the weight by w_k, the overall
+  quality is then
+  q = sum_k w_k*q_k.
+
+  Notice that this is particularly useful for graphs containing negative links.
+  When separating the graph in two graphs, the one containing only the positive
+  links, and the other only the negative link, by supplying a negative weight to
+  the latter layer, we try to find relatively many positive links within a
+  community and relatively many negative links between communities.
+
+  Keyword arguments:
+
+  layers
+    A list of all layers containing the additional information. See ``Layer``
+    class for more information.
+
+  consider_comms=ALL_NEIGH_COMMS
+    This parameter determines which communities to consider when moving a node.
+
+    ALL_COMMS
+      Consider all communities always.
+
+    ALL_NEIGH_COMMS
+      Consider all communities of the neighbours
+
+    RAND_COMM
+      Consider only a single random community
+
+    ALL_NEIGH_COMMS
+      Consider only a single random community of the neighbours. Notice that
+      this is sampled from the set of all neighbours so that the communities are
+      sampled with respective frequency.
+
+    In ordinary cases it is usually not necessary to alter this parameter. The
+    default choice of considering all communities of the neighbours works
+    relatively well, and is relatively fast. However, in the case of negative
+    weights, it may be better to move a node to a community to which it is not
+    connected, so that one would need to consider all communities.
+    Alternatively, by only selecting a single random community from the
+    neighbours to consider, one can considerably speed up the algorithm, without
+    loosing too much quality.
+
+  returns: membership vector and the quality
+
+  We return the membership vector instead of a partition, because the latter
+  needs an underlying graph.
+  """
+  args = [];
+  for layer in layers:
+    if layer.weight is not None:
+      if isinstance(layer.weight, str):
+        layer.weight = layer.graph.es[layer.weight];
+      else:
+        # Make sure it is a list
+        layer.weight = list(layer.weight);
+    args.append((__get_py_capsule(layer.graph), layer.method,
+           layer.layer_weight, layer.initial_membership, layer.weight,
+           layer.resolution_parameter));
+  membership, quality = _c_louvain._find_partition_multiplex(args, consider_comms);
+  return membership, quality;
+
+def find_partition(graph, method, initial_membership=None, weight=None,
+    resolution_parameter=1.0, consider_comms=ALL_NEIGH_COMMS):
+  """
+  Method for detecting communities using the Louvain algorithm. This functions
+  finds the optimal partition given the specified method. For the various possible
+  methods see package documentation.
+
+  Keyword arguments:
+
+  graph
+    The graph for which to find the optimal partition.
+
+  method
+    The type of partition which will be used during optimisation.
+
+  initial_membership=None
+    If provided, the optimisation will start with this initial membership.
+    Should be a list that contains any unique identified for a community, which
+    is converted to a numeric representation. Since communities can never be
+    split, the number of communities in this initial partition provides an upper
+    bound.
+
+  weight=None
+    If provided, indicates the edge attribute to use as a weight. (N.B. note
+    that Significance is not suited for weighted graphs).
+
+  resolution_parameter=1.0
+    For those methods that use a resolution parameter, this is indicated here.
+
+  consider_comms=ALL_NEIGH_COMMS
+    This parameter determines which communities to consider when moving a node.
+
+    ALL_COMMS
+      Consider all communities always.
+
+    ALL_NEIGH_COMMS
+      Consider all communities of the neighbours
+
+    RAND_COMM
+      Consider only a single random community
+
+    ALL_NEIGH_COMMS
+      Consider only a single random community of the neighbours. Notice that
+      this is sampled from the set of all neighbours so that the communities are
+      sampled with respective frequency.
+
+    In ordinary cases it is usually not necessary to alter this parameter. The
+    default choice of considering all communities of the neighbours works
+    relatively well, and is relatively fast. However, in the case of negative
+    weights, it may be better to move a node to a community to which it is not
+    connected, so that one would need to consider all communities.
+    Alternatively, by only selecting a single random community from the
+    neighbours to consider, one can considerably speed up the algorithm, without
+    loosing too much quality.
+
+  The quality of the partition, as measured by the indicated method is
+  provided in the returned partition as partition.quality.
+
+  returns: optimized partition."""
+  pygraph_t = __get_py_capsule(graph);
+  if weight is not None:
+    if isinstance(weight, str):
+      weight = graph.es[weight];
+    else:
+      # Make sure it is a list
+      weight = list(weight);
+  if initial_membership is not None:
+    gen = _ig.UniqueIdGenerator();
+    initial_membership = [gen[m] for m in initial_membership];
+  membership, quality = _c_louvain._find_partition(pygraph_t, method, initial_membership, weight, resolution_parameter, consider_comms);
+  partition = _ig.VertexClustering(graph, membership);
+  partition.quality = quality;
+  return partition;
