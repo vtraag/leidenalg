681629f03e1d8c834f8b1580930bc381a18d3a75
vtraag <vincent@traag.net>
2015-12-15 14:15:06 +0100

    Corrected SignificanceVertexPartition counting the number of possible edges."

--- src/SignificanceVertexPartition.cpp
+++ src/SignificanceVertexPartition.cpp
@@ -132,16 +132,9 @@ double SignificanceVertexPartition::quality()
     size_t n_c = this->csize(c);
     double m_c = this->total_weight_in_comm(c);
     double p_c = 0.0;
-    if (n_c > 1)
-    {
-      size_t N_c = this->graph->possible_edges(n_c);
+    size_t N_c = this->graph->possible_edges(n_c);
+    if (N_c > 0)
       p_c = m_c/N_c;
-      #ifdef DEBUG
-        cerr << "\t" << "c=" << c << ", n_c=" << n_c << ", m_c=" << m_c << ", N_c=" << N_c
-           << ", p_c=" << p_c << ", p=" << p << ", KLL=" << KLL(p_c, p) << "." << endl;
-      #endif
-      S += N_c*KL(p_c, p);
-    }
     #ifdef DEBUG
     else
     {
