63222f4fb333efa12b85090006c20bd42577377d
vtraag <vincent@traag.net>
2016-10-19 17:31:09 +0200

    Added some tests.

--- tests/test_Optimiser.py
+++ tests/test_Optimiser.py
@@ -13,8 +13,7 @@ class OptimiserTest(unittest.TestCase):
     self.optimiser.move_nodes(partition, consider_comms=louvain.ALL_NEIGH_COMMS);
     self.assertListEqual(
         partition.sizes(), [100],
-        places=5,
-        msg="Quality of CPMPartition(resolution_parameter=0.5) of complete graph after move nodes is not equal to 0.0.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after move nodes incorrect.");
 
   def test_merge_nodes(self):
     G = ig.Graph.Full(100);
@@ -22,8 +21,7 @@ class OptimiserTest(unittest.TestCase):
     self.optimiser.merge_nodes(partition, consider_comms=louvain.ALL_NEIGH_COMMS);
     self.assertListEqual(
         partition.sizes(), [100],
-        places=5,
-        msg="Quality of CPMPartition(resolution_parameter=0.5) of complete graph after move nodes is not equal to 0.0.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes incorrect.");
 
   def test_diff_move_node_optimality(self):
     G = ig.Graph.Erdos_Renyi(100, p=5./100, directed=False, loops=False);
@@ -37,6 +35,24 @@ class OptimiserTest(unittest.TestCase):
           partition.diff_move(v.index, c), 0,
           msg="Was able to move a node to a better community, violating node optimality.");
 
+  def test_optimiser(self):
+    G = reduce(ig.Graph.disjoint_union, (ig.Graph.Tree(10, 3, mode=ig.TREE_UNDIRECTED) for i in range(10)));
+    partition = louvain.CPMVertexPartition(G, resolution_parameter=0);
+    self.optimiser.consider_comms=louvain.ALL_NEIGH_COMMS;
+    self.optimiser.optimise_partition(partition);
+    self.assertListEqual(
+        partition.sizes(), 10*[10],
+        msg="After optimising partition failed to find different components with CPMVertexPartition(resolution_parameter=0)");
+
+  def test_neg_weight_bipartite(self):
+    G = ig.Graph.Full_Bipartite(50, 50);
+    G.es['weight'] = -0.1;
+    partition = louvain.CPMVertexPartition(G, resolution_parameter=-0.1, weight='weight');
+    self.optimiser.consider_comms=louvain.ALL_COMMS;
+    self.optimiser.optimise_partition(partition);
+    self.assertListEqual(
+        partition.sizes(), 2*[50],
+        msg="After optimising partition failed to find bipartite structure with CPMVertexPartition(resolution_parameter=-0.1)");
 
 #%%
 if __name__ == '__main__':
