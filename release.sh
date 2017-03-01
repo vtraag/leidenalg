461105123b3f49de8aa256ff99702f769d839f3a
Vincent Traag <vincent@traag.net>
2017-03-01 15:21:31 +0100

    Merge branch 'conda_install'

--- release.sh
+++ release.sh
@@ -11,6 +11,13 @@ python setup.py sdist
 #python2.7 setup.py bdist_egg upload --sign
 #python3.4 setup.py bdist_egg upload --sign
 
+# Conda packages
+~/miniconda2/bin/python setup.py --no-pkg-config bdist_conda
+~/miniconda2/bin/anaconda upload -i ~/miniconda2/conda-bld/linux-64/louvain-0.5.3-py27_0.tar.bz2
+
+~/miniconda3/bin/python setup.py --no-pkg-config bdist_conda
+~/miniconda3/bin/anaconda upload -i ~/miniconda3/conda-bld/linux-64/louvain-0.5.3-py35_0.tar.bz2
+
 cd -
 
 if [ ! -d "dist/" ]; then
