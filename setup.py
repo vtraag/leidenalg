d3ecc5fce5c48d1723b8eddabbdf632dbb87f993
vtraag <vincent@traag.net>
2017-11-10 22:57:33 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -605,7 +605,7 @@ louvain_ext = Extension('louvain._c_louvain',
                     sources = glob.glob(os.path.join('src', '*.cpp')),
                     include_dirs=['include']);
 
-version = '0.6.0';
+version = '0.6.1';
 options =  dict(
   name = 'louvain',
   version = version,
