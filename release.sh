ccc002bc0929869faa54e04b9d497ac7a6d533b1
vtraag <vincent@traag.net>
2017-11-12 13:47:29 +0100

    Updated some release things, and corrected a minor documentation issue.

--- release.sh
+++ release.sh
@@ -6,14 +6,19 @@ cp -R * /tmp/louvain/
 cd /tmp/louvain/
 
 # source dist 
+source ~/anaconda3/bin/activate root
 python setup.py sdist
 #python setup.py sdist upload --sign
 #python2.7 setup.py bdist_egg upload --sign
 #python3.4 setup.py bdist_egg upload --sign
 
+# docs will be built automatically by readthedocs.org
+# python setup.py build_sphinx
+
 # Conda packages
-~/miniconda2/bin/python setup.py --no-pkg-config bdist_conda
-~/miniconda2/bin/anaconda upload -i ~/miniconda2/conda-bld/linux-64/louvain-0.5.3-py27_0.tar.bz2
+conda config --set anaconda_upload yes
+source ~/anaconda3/bin/activate root
+python setup.py --no-pkg-config bdist_conda
 
 ~/miniconda3/bin/python setup.py --no-pkg-config bdist_conda
 ~/miniconda3/bin/anaconda upload -i ~/miniconda3/conda-bld/linux-64/louvain-0.5.3-py35_0.tar.bz2
