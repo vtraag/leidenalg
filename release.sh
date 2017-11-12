ace4b4981ee443725a00d1b96175a2cc79bb55ac
vtraag <vincent@traag.net>
2017-11-12 14:07:57 +0100

    Updated release script to reflect release.

--- release.sh
+++ release.sh
@@ -1,29 +1,49 @@
 #!/bin/sh
 
-mkdir /tmp/louvain/
-cp -R * /tmp/louvain/
-
-cd /tmp/louvain/
-
-# source dist 
-python setup.py sdist
-#python setup.py sdist upload --sign
-#python2.7 setup.py bdist_egg upload --sign
-#python3.4 setup.py bdist_egg upload --sign
-
-# Conda packages
-~/miniconda2/bin/python setup.py --no-pkg-config bdist_conda
-~/miniconda2/bin/anaconda upload -i ~/miniconda2/conda-bld/linux-64/louvain-0.5.3-py27_0.tar.bz2
-
-~/miniconda3/bin/python setup.py --no-pkg-config bdist_conda
-~/miniconda3/bin/anaconda upload -i ~/miniconda3/conda-bld/linux-64/louvain-0.5.3-py35_0.tar.bz2
-
-cd -
-
-if [ ! -d "dist/" ]; then
-  mkdir dist/
+if [ "$1" = "release" ]; then
+  echo "Making release, uploading to PyPi and Anaconda..."
+  # source dist 
+  source ~/anaconda2/bin/activate root
+  python setup.py sdist upload --sign
+  python setup.py bdist_egg upload --sign
+  
+  source ~/anaconda3/bin/activate root
+  python setup.py bdist_egg upload --sign
+  
+  # docs will be built automatically by readthedocs.org
+  # python setup.py build_sphinx
+  
+  # Conda packages
+  ~/anaconda2/bin/conda config --set anaconda_upload yes
+  source ~/anaconda2/bin/activate root
+  python setup.py --no-pkg-config bdist_conda
+  
+  ~/anaconda3/bin/conda config --set anaconda_upload yes
+  source ~/anaconda3/bin/activate root
+  python setup.py --no-pkg-config bdist_conda
+else
+  echo "Dry run, not uploading anything..."
+  # source dist 
+  source ~/anaconda2/bin/activate root
+  python setup.py sdist
+  python setup.py bdist_egg
+  
+  source ~/anaconda3/bin/activate root
+  python setup.py sdist
+  python setup.py sdist
+  python setup.py bdist_egg
+  
+  # docs will be built automatically by readthedocs.org
+  # but let's test if there is not anything going wrong
+  python setup.py build_sphinx
+  
+  # Conda packages
+  ~/anaconda2/bin/conda config --set anaconda_upload no
+  source ~/anaconda2/bin/activate root
+  python setup.py --no-pkg-config bdist_conda
+  
+  ~/anaconda3/bin/conda config --set anaconda_upload no
+  source ~/anaconda3/bin/activate root
+  python setup.py --no-pkg-config bdist_conda
 fi
 
-cp /tmp/louvain/dist/* dist/
-
-rm -rf /tmp/louvain/
