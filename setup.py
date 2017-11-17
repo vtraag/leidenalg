84fae42951bce2691da5960b2588f72cb147b3a6
Vincent Traag <vincent@traag.net>
2017-11-17 12:42:45 +0100

    Changed around long description on PyPi.

--- setup.py
+++ setup.py
@@ -609,11 +609,11 @@ options =  dict(
     """
  Louvain is a general algorithm for methods of community detection in large networks.
 
- Please refer to http://louvain-igraph.readthedocs.io/en/latest
- for more extensive documentation.
+ Please refer to the `documentation <http://louvain-igraph.readthedocs.io/en/latest>`_
+ for more details.
 
- The source code of this package is hosted at https://github.com/vtraag/louvain-igraph.
- Issues and bug reports are welcome at https://github.com/vtraag/louvain-igraph/issues
+ The source code of this package is hosted at `GitHub <https://github.com/vtraag/louvain-igraph>`_.
+ Issues and bug reports are welcome at https://github.com/vtraag/louvain-igraph/issues.
     """,
   license = 'GPLv3+',
   url = 'https://github.com/vtraag/louvain-igraph',
