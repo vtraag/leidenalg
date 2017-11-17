8a8c848eba379bc6b93d90d296d835139e78385e
Vincent Traag <vincent@traag.net>
2017-11-17 09:59:49 +0100

    Forgot to set upload to yes for release script.

--- release_ms.bat
+++ release_ms.bat
@@ -6,13 +6,13 @@ IF "%1%"=="release" (
     :: Build for Python 2
     call %userprofile%\AppData\Local\Continuum\anaconda2\Scripts\activate
     python setup.py build bdist_wheel
-    conda config --set anaconda_upload no
+    conda config --set anaconda_upload yes
     python setup.py build bdist_conda
 
     :: Build for Python 3
     call %userprofile%\AppData\Local\Continuum\anaconda3\Scripts\activate
     python setup.py build bdist_wheel
-    conda config --set anaconda_upload no
+    conda config --set anaconda_upload yes
     python setup.py build bdist_conda
 
     :: Upload to PyPi using twine
