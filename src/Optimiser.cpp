2843e4b91c649fdda904594bfd05f771fb209314
ragibson <ryan.alexander.gibson@gmail.com>
2020-07-14 00:44:37 -0400

    Decrease amount of debug output, targeting fixed nodes test

--- src/Optimiser.cpp
+++ src/Optimiser.cpp
@@ -600,9 +600,11 @@ double Optimiser::move_nodes(vector<MutableVertexPartition*> partitions, vector<
 
       if (possible_improv > max_improv)
       {
-        printf("possible_improv=%g > max_improv=%g ?\n", possible_improv, max_improv);
-        printf("possible_improv=%a > max_improv=%a ?\n", possible_improv, max_improv);
-        printf("(gt=%d, eq=%d, lt=%d)\n", possible_improv > max_improv, possible_improv == max_improv, possible_improv < max_improv);
+        if (n == 3) {
+          printf("possible_improv=%g > max_improv=%g ?\n", possible_improv, max_improv);
+          printf("possible_improv=%a > max_improv=%a ?\n", possible_improv, max_improv);
+          printf("(gt=%d, eq=%d, lt=%d)\n", possible_improv > max_improv, possible_improv == max_improv, possible_improv < max_improv);
+        }
         max_comm = comm;
         max_improv = possible_improv;
       }
@@ -639,9 +641,11 @@ double Optimiser::move_nodes(vector<MutableVertexPartition*> partitions, vector<
         #endif
         if (possible_improv > max_improv)
         {
-          printf("possible_improv=%g > max_improv=%g ?\n", possible_improv, max_improv);
-          printf("possible_improv=%a > max_improv=%a ?\n", possible_improv, max_improv);
-          printf("(gt=%d, eq=%d, lt=%d)\n", possible_improv > max_improv, possible_improv == max_improv, possible_improv < max_improv);
+          if (n == 3) {
+            printf("possible_improv=%g > max_improv=%g ?\n", possible_improv, max_improv);
+            printf("possible_improv=%a > max_improv=%a ?\n", possible_improv, max_improv);
+            printf("(gt=%d, eq=%d, lt=%d)\n", possible_improv > max_improv, possible_improv == max_improv, possible_improv < max_improv);
+          }
           max_improv = possible_improv;
           max_comm = comm;
         }
