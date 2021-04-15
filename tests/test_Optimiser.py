889210eb1256152b2d60e8e4fbbc9f72665978ec
Vincent Traag <vincent@traag.net>
2021-04-15 09:47:48 +0200

    Removed trailing ;.

--- tests/test_Optimiser.py
+++ tests/test_Optimiser.py
@@ -3,7 +3,7 @@ import igraph as ig
 import leidenalg
 
 import sys
-PY3 = (sys.version > '3');
+PY3 = (sys.version > '3')
 
 if PY3:
   from functools import reduce
@@ -11,24 +11,24 @@ if PY3:
 class OptimiserTest(unittest.TestCase):
 
   def setUp(self):
-    self.optimiser = leidenalg.Optimiser();
+    self.optimiser = leidenalg.Optimiser()
 
   def test_move_nodes(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5);
-    self.optimiser.move_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5)
+    self.optimiser.move_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [100],
-        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after move nodes incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after move nodes incorrect.")
 
   def test_move_nodes_with_max_comm_size(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5);
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5)
     self.optimiser.max_comm_size = 17
-    self.optimiser.move_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+    self.optimiser.move_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [17, 17, 17, 17, 17, 15],
-        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after move nodes (max_comm_size=17) incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after move nodes (max_comm_size=17) incorrect.")
 
   def test_move_nodes_with_fixed(self):
     # One edge plus singleton, but the two connected nodes are fixed
@@ -36,76 +36,76 @@ class OptimiserTest(unittest.TestCase):
     is_membership_fixed = [True, False, True]
     partition = leidenalg.CPMVertexPartition(
             G,
-            resolution_parameter=0.1);
-    self.optimiser.move_nodes(partition, is_membership_fixed=is_membership_fixed, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+            resolution_parameter=0.1)
+    self.optimiser.move_nodes(partition, is_membership_fixed=is_membership_fixed, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [1, 1, 1],
-        msg="CPMVertexPartition(resolution_parameter=0.1) of one edge plus singleton after move nodes with fixed nodes is incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.1) of one edge plus singleton after move nodes with fixed nodes is incorrect.")
 
   def test_merge_nodes(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5);
-    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5)
+    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [100],
-        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes incorrect.")
     self.assertEqual(
         partition.total_weight_in_all_comms(),
         G.ecount(),
-        msg="total_weight_in_all_comms not equal to ecount of graph.");
+        msg="total_weight_in_all_comms not equal to ecount of graph.")
 
   def test_merge_nodes_with_max_comm_size(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5);
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5)
     self.optimiser.max_comm_size = 17
-    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [17, 17, 17, 17, 17, 15],
-        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes (max_comm_size=17) incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes (max_comm_size=17) incorrect.")
 
   def test_diff_move_node_optimality(self):
-    G = ig.Graph.Erdos_Renyi(100, p=5./100, directed=False, loops=False);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.1);
+    G = ig.Graph.Erdos_Renyi(100, p=5./100, directed=False, loops=False)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.1)
     while 0 < self.optimiser.move_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS):
-      pass;
+      pass
     for v in G.vs:
-      neigh_comms = set(partition.membership[u.index] for u in v.neighbors());
+      neigh_comms = set(partition.membership[u.index] for u in v.neighbors())
       for c in neigh_comms:
         self.assertLessEqual(
           partition.diff_move(v.index, c), 1e-10, # Allow for a small difference up to rounding error.
-          msg="Was able to move a node to a better community, violating node optimality.");
+          msg="Was able to move a node to a better community, violating node optimality.")
 
   def test_optimiser(self):
-    G = reduce(ig.Graph.disjoint_union, (ig.Graph.Tree(10, 3, mode=ig.TREE_UNDIRECTED) for i in range(10)));
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0);
-    self.optimiser.consider_comms=leidenalg.ALL_NEIGH_COMMS;
-    self.optimiser.optimise_partition(partition);
+    G = reduce(ig.Graph.disjoint_union, (ig.Graph.Tree(10, 3, mode=ig.TREE_UNDIRECTED) for i in range(10)))
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0)
+    self.optimiser.consider_comms=leidenalg.ALL_NEIGH_COMMS
+    self.optimiser.optimise_partition(partition)
     self.assertListEqual(
         partition.sizes(), 10*[10],
-        msg="After optimising partition failed to find different components with CPMVertexPartition(resolution_parameter=0)");
+        msg="After optimising partition failed to find different components with CPMVertexPartition(resolution_parameter=0)")
 
   def test_optimiser_with_max_comm_size(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0);
-    self.optimiser.consider_comms=leidenalg.ALL_NEIGH_COMMS;
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0)
+    self.optimiser.consider_comms=leidenalg.ALL_NEIGH_COMMS
     self.optimiser.max_comm_size = 10
-    self.optimiser.optimise_partition(partition);
+    self.optimiser.optimise_partition(partition)
     self.assertListEqual(
         partition.sizes(), 10*[10],
-        msg="After optimising partition (max_comm_size=10) failed to find different components with CPMVertexPartition(resolution_parameter=0)");
+        msg="After optimising partition (max_comm_size=10) failed to find different components with CPMVertexPartition(resolution_parameter=0)")
 
   def test_optimiser_split_with_max_comm_size(self):
-    G = ig.Graph.Full(100);
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5);
-    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS);
+    G = ig.Graph.Full(100)
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=0.5)
+    self.optimiser.merge_nodes(partition, consider_comms=leidenalg.ALL_NEIGH_COMMS)
     self.assertListEqual(
         partition.sizes(), [100],
-        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes incorrect.");
+        msg="CPMVertexPartition(resolution_parameter=0.5) of complete graph after merge nodes incorrect.")
     self.optimiser.max_comm_size = 10
-    self.optimiser.optimise_partition(partition);
+    self.optimiser.optimise_partition(partition)
     self.assertListEqual(
         partition.sizes(), 10*[10],
-        msg="After optimising partition (max_comm_size=10) failed to find different components with CPMVertexPartition(resolution_parameter=0.5)");
+        msg="After optimising partition (max_comm_size=10) failed to find different components with CPMVertexPartition(resolution_parameter=0.5)")
 
   def test_optimiser_with_is_membership_fixed(self):
       G = ig.Graph.Full(3)
@@ -148,28 +148,28 @@ class OptimiserTest(unittest.TestCase):
 
 
   def test_neg_weight_bipartite(self):
-    G = ig.Graph.Full_Bipartite(50, 50);
-    G.es['weight'] = -0.1;
-    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=-0.1, weights='weight');
-    self.optimiser.consider_comms=leidenalg.ALL_COMMS;
-    self.optimiser.optimise_partition(partition);
+    G = ig.Graph.Full_Bipartite(50, 50)
+    G.es['weight'] = -0.1
+    partition = leidenalg.CPMVertexPartition(G, resolution_parameter=-0.1, weights='weight')
+    self.optimiser.consider_comms=leidenalg.ALL_COMMS
+    self.optimiser.optimise_partition(partition)
     self.assertListEqual(
         partition.sizes(), 2*[50],
-        msg="After optimising partition failed to find bipartite structure with CPMVertexPartition(resolution_parameter=-0.1)");
+        msg="After optimising partition failed to find bipartite structure with CPMVertexPartition(resolution_parameter=-0.1)")
 
   def test_resolution_profile(self):
-    G = ig.Graph.Famous('Zachary');
-    profile = self.optimiser.resolution_profile(G, leidenalg.CPMVertexPartition, resolution_range=(0,1));
+    G = ig.Graph.Famous('Zachary')
+    profile = self.optimiser.resolution_profile(G, leidenalg.CPMVertexPartition, resolution_range=(0,1))
     self.assertListEqual(
       profile[0].sizes(), [G.vcount()],
-      msg="Resolution profile incorrect: at resolution 0, not equal to a single community for CPM.");
+      msg="Resolution profile incorrect: at resolution 0, not equal to a single community for CPM.")
     self.assertListEqual(
       profile[-1].sizes(), [1]*G.vcount(),
-      msg="Resolution profile incorrect: at resolution 1, not equal to a singleton partition for CPM.");
+      msg="Resolution profile incorrect: at resolution 1, not equal to a singleton partition for CPM.")
 
 #%%
 if __name__ == '__main__':
   #%%
-  unittest.main(verbosity=3);
-  suite = unittest.TestLoader().discover('.');
-  unittest.TextTestRunner(verbosity=1).run(suite);
+  unittest.main(verbosity=3)
+  suite = unittest.TestLoader().discover('.')
+  unittest.TextTestRunner(verbosity=1).run(suite)
--- tests/test_VertexPartition.py
+++ tests/test_VertexPartition.py
@@ -7,12 +7,12 @@ from copy import deepcopy
 from ddt import ddt, data, unpack
 
 import sys
-PY3 = (sys.version > '3');
+PY3 = (sys.version > '3')
 #%%
 
 def name_object(obj, name):
-  obj.__name__ = name;
-  return obj;
+  obj.__name__ = name
+  return obj
 
 graphs = [
     ###########################################################################
@@ -58,7 +58,7 @@ graphs = [
                 'Lattice_undirected'),
     name_object(ig.Graph.Lattice([100], nei=3, directed=True, mutual=False, circular=True),
                 'Lattice_directed')
-    ];
+    ]
 
 bipartite_graph = name_object(
     ig.Graph.Bipartite([0, 0, 0, 0, 1, 1, 1, 1],
@@ -75,91 +75,88 @@ bipartite_graph = name_object(
               'bipartite_example')
 
 def make_weighted(G):
-  m = G.ecount();
-  if PY3:
-    G.es['weight'] = [random.random() for i in range(G.ecount())];
-  else:
-    G.es['weight'] = [random.random() for i in xrange(G.ecount())];
-  G.__name__ += '_weighted';
-  return G;
+  m = G.ecount()
+  G.es['weight'] = [random.random() for i in range(G.ecount())]
+  G.__name__ += '_weighted'
+  return G
 
-graphs += [make_weighted(H) for H in graphs];
+graphs += [make_weighted(H) for H in graphs]
 
 class BaseTest:
   @ddt
   class MutableVertexPartitionTest(unittest.TestCase):
 
     def setUp(self):
-      self.optimiser = leidenalg.Optimiser();
+      self.optimiser = leidenalg.Optimiser()
 
     @data(*graphs)
     def test_move_nodes(self, graph):
       if 'weight' in graph.es.attributes() and self.partition_type == leidenalg.SignificanceVertexPartition:
-        raise unittest.SkipTest('Significance doesn\'t handle weighted graphs');
+        raise unittest.SkipTest('Significance doesn\'t handle weighted graphs')
 
       if 'weight' in graph.es.attributes():
-        partition = self.partition_type(graph, weights='weight');
+        partition = self.partition_type(graph, weights='weight')
       else:
-        partition = self.partition_type(graph);
+        partition = self.partition_type(graph)
       for v in range(graph.vcount()):
         if graph.degree(v) >= 1:
-          u = graph.neighbors(v)[0];
-          diff = partition.diff_move(v, partition.membership[u]);
-          q1 = partition.quality();
-          partition.move_node(v, partition.membership[u]);
-          q2 = partition.quality();
+          u = graph.neighbors(v)[0]
+          diff = partition.diff_move(v, partition.membership[u])
+          q1 = partition.quality()
+          partition.move_node(v, partition.membership[u])
+          q2 = partition.quality()
           self.assertAlmostEqual(
               q2 - q1,
               diff,
               places=5,
               msg="Difference in quality ({0}) not equal to calculated difference ({1})".format(
-              q2 - q1, diff));
+              q2 - q1, diff))
 
     @data(*graphs)
     def test_aggregate_partition(self, graph):
       if 'weight' in graph.es.attributes() and self.partition_type != leidenalg.SignificanceVertexPartition:
-        partition = self.partition_type(graph, weights='weight');
+        partition = self.partition_type(graph, weights='weight')
       else:
-        partition = self.partition_type(graph);
-      self.optimiser.move_nodes(partition);
-      aggregate_partition = partition.aggregate_partition();
+        partition = self.partition_type(graph)
+      self.optimiser.move_nodes(partition)
+      aggregate_partition = partition.aggregate_partition()
       self.assertAlmostEqual(
           partition.quality(),
           aggregate_partition.quality(),
           places=5,
-          msg='Quality not equal for aggregate partition.');
-      self.optimiser.move_nodes(aggregate_partition);
-      partition.from_coarse_partition(aggregate_partition);
+          msg='Quality not equal for aggregate partition.')
+      self.optimiser.move_nodes(aggregate_partition)
+      partition.from_coarse_partition(aggregate_partition)
       self.assertAlmostEqual(
           partition.quality(),
           aggregate_partition.quality(),
           places=5,
-          msg='Quality not equal from coarser partition.');
+          msg='Quality not equal from coarser partition.')
 
     @data(*graphs)
     def test_total_weight_in_all_comms(self, graph):
       if 'weight' in graph.es.attributes() and self.partition_type != leidenalg.SignificanceVertexPartition:
-        partition = self.partition_type(graph, weights='weight');
+        partition = self.partition_type(graph, weights='weight')
       else:
-        partition = self.partition_type(graph);
-      self.optimiser.optimise_partition(partition);
-      s = sum([partition.total_weight_in_comm(c) for c,_ in enumerate(partition)]);
+        partition = self.partition_type(graph)
+      self.optimiser.optimise_partition(partition)
+      s = sum([partition.total_weight_in_comm(c) for c,_ in enumerate(partition)])
       self.assertAlmostEqual(
         s,
         partition.total_weight_in_all_comms(),
         places=5,
         msg='Total weight in all communities ({0}) not equal to the sum of the weight in all communities ({1}).'.format(
           s, partition.total_weight_in_all_comms())
-        );
+        )
 
     @data(*graphs)
     def test_copy(self, graph):
       if 'weight' in graph.es.attributes() and self.partition_type != leidenalg.SignificanceVertexPartition:
-        partition = self.partition_type(graph, weights='weight');
+        partition = self.partition_type(graph, weights='weight')
       else:
-        partition = self.partition_type(graph);
+        partition = self.partition_type(graph)
 
-      self.optimiser.optimise_partition(partition);
+      self.optimiser.optimise_partition(partition)
 
       partition2 = deepcopy(partition)
 
@@ -186,23 +183,23 @@ class BaseTest:
 
 class ModularityVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(ModularityVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.ModularityVertexPartition;
+    super(ModularityVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.ModularityVertexPartition
 
 class RBERVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(RBERVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.RBERVertexPartition;
+    super(RBERVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.RBERVertexPartition
 
 class RBConfigurationVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(RBConfigurationVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.RBConfigurationVertexPartition;
+    super(RBConfigurationVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.RBConfigurationVertexPartition
 
 class CPMVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(CPMVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.CPMVertexPartition;
+    super(CPMVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.CPMVertexPartition
 
   def test_Bipartite(self):
     graph = bipartite_graph
@@ -215,17 +212,17 @@ class CPMVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
 
 class SurpriseVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(SurpriseVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.SurpriseVertexPartition;
+    super(SurpriseVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.SurpriseVertexPartition
 
 class SignificanceVertexPartitionTest(BaseTest.MutableVertexPartitionTest):
   def setUp(self):
-    super(SignificanceVertexPartitionTest, self).setUp();
-    self.partition_type = leidenalg.SignificanceVertexPartition;
+    super(SignificanceVertexPartitionTest, self).setUp()
+    self.partition_type = leidenalg.SignificanceVertexPartition
 
 #%%
 if __name__ == '__main__':
   #%%
-  unittest.main(verbosity=3);
-  suite = unittest.TestLoader().discover('.');
-  unittest.TextTestRunner(verbosity=1).run(suite);
+  unittest.main(verbosity=3)
+  suite = unittest.TestLoader().discover('.')
+  unittest.TextTestRunner(verbosity=1).run(suite)
