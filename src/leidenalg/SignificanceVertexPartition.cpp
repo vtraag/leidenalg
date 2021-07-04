5adafb1e10ccae91f3efbddb4ba5e272cb0a08eb
Vincent Traag <vincent@traag.net>
2021-07-04 19:28:29 +0200

    Revert "Significance: Remove DEBUG def."
    This reverts commit ad6c79fd45813e4dbc65f8fc4307bf96a77e8930.

--- src/leidenalg/SignificanceVertexPartition.cpp
+++ src/leidenalg/SignificanceVertexPartition.cpp
@@ -29,6 +29,7 @@ SignificanceVertexPartition* SignificanceVertexPartition::create(Graph* graph, v
 SignificanceVertexPartition::~SignificanceVertexPartition()
 { }
 
+#define DEBUG
 double SignificanceVertexPartition::diff_move(size_t v, size_t new_comm)
 {
   #ifdef DEBUG
@@ -126,6 +127,7 @@ double SignificanceVertexPartition::diff_move(size_t v, size_t new_comm)
   #endif
   return diff;
 }
+#undef DEBUG
 
 /********************************************************************************
    Calculate the significance of the partition.
