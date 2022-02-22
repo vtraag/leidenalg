cae61cbf5035c6baded7f28fc6d384fff2eaed69
Vincent Traag <vincent@traag.net>
2022-02-22 10:15:28 +0100

    doc: Clarified node_sizes for other partition types.

--- src/leidenalg/VertexPartition.py
+++ src/leidenalg/VertexPartition.py
@@ -523,9 +523,11 @@ class SurpriseVertexPartition(MutableVertexPartition):
       Weights of edges. Can be either an iterable or an edge attribute.
 
     node_sizes : list of int, or vertex attribute
-      Sizes of nodes are necessary to know the size of communities in aggregate
-      graphs. Usually this is set to 1 for all nodes, but in specific cases
-      this could be changed.
+      The quality function takes into account the size of a community, which
+      is defined as the sum over the sizes of each individual node. By default, 
+      the node sizes are set to 1, meaning that the size of a community equals 
+      the number of nodes of a community. If a node already represents an 
+      aggregation, this could be reflect in its node size.
     """
     if initial_membership is not None:
       initial_membership = list(initial_membership)
@@ -603,9 +605,11 @@ class SignificanceVertexPartition(MutableVertexPartition):
       singleton partition.
 
     node_sizes : list of int, or vertex attribute
-      Sizes of nodes are necessary to know the size of communities in aggregate
-      graphs. Usually this is set to 1 for all nodes, but in specific cases
-      this could be changed.
+      The quality function takes into account the size of a community, which
+      is defined as the sum over the sizes of each individual node. By default, 
+      the node sizes are set to 1, meaning that the size of a community equals 
+      the number of nodes of a community. If a node already represents an 
+      aggregation, this could be reflect in its node size.
     """
     if initial_membership is not None:
       initial_membership = list(initial_membership)
